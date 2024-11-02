a884a3851ffe5ee9b44cab2c8198388e2ceb9f00
kristophernorth <kristopher.north@gmail.com>
2024-11-01 18:02:58 -0600

    auto upgrade functions

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 //#region STATE
 let cheese = 0;
 
+let clickUpgrades = [
+  {
+    name: 'knife',
+    price: 10,
+    quantity: 0,
+    bonus: 1
+  },
+  {
+    name: 'drill',
+    price: 15,
+    quantity: 0,
+    bonus: 10
+  }
+];
+
+let autoUpgrades = [
+  {
+    name: 'mousetronaut',
+    price: 25,
+    quantity: 0,
+    bonus: 100
+  },
+  {
+    name: 'rover',
+    price: 30,
+    quantity: 0,
+    bonus: 500
+  }
+];
+
 //#endregion
 
 
@@ -10,6 +40,50 @@ function increaseCheese() {
   cheese += 1
   console.log(cheese)
   drawCheese()
+}
+
+function buyKnife() {
+  console.log(clickUpgrades[0].name)
+  if (cheese >= clickUpgrades[0].price) {
+    clickUpgrades[0].quantity += 1
+    cheese -= clickUpgrades[0].price
+    console.log(clickUpgrades[0])
+    drawKnives()
+    drawCheese()
+  }
+}
+
+function buyDrill() {
+  console.log(clickUpgrades[1].name)
+  if (cheese >= clickUpgrades[1].price) {
+    clickUpgrades[1].quantity += 1
+    cheese -= clickUpgrades[1].price
+    console.log(clickUpgrades[1])
+    drawDrills()
+    drawCheese()
+  }
+}
+
+function buyMousetronaut() {
+  console.log(autoUpgrades[0].name)
+  if (cheese >= autoUpgrades[0].price) {
+    autoUpgrades[0].quantity += 1
+    cheese -= autoUpgrades[0].price
+    console.log(autoUpgrades[0])
+    drawMousetronauts()
+    drawCheese()
+  }
+}
+
+function buySpaceStations() {
+  console.log(autoUpgrades[1].name)
+  if (cheese >= autoUpgrades[1].price) {
+    autoUpgrades[1].quantity += 1
+    cheese -= autoUpgrades[1].price
+    console.log(autoUpgrades[1])
+    drawSpaceStations()
+    drawCheese()
+  }
 }
 
 
@@ -24,5 +98,16 @@ function drawCheese() {
   increaseCheeseElm.innerText = cheese.toString()
 }
 
+function drawKnives() {
+  const increaseKnivesElm = document.getElementById('knifeTotal')
+  console.log('knifeTotal', increaseKnivesElm)
+  increaseKnivesElm.innerText = clickUpgrades[0].quantity.toString()
+}
+
+function drawDrills() {
+  const increaseDrillsElm = document.getElementById('drillTotal')
+  console.log('drillTotal', increaseDrillsElm)
+  increaseDrillsElm.innerText = clickUpgrades[1].quantity.toString()
+}
 
 //#endregion
