7f63fe3bb557fec99b5334def590e801abdec6bd
kristophernorth <kristopher.north@gmail.com>
2024-11-04 19:07:48 -0700

    Finished, everything working

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ let autoUpgrades = [
 
 
 //#region LOGIC
-// NOTE - can I do this much easier with a for-loop??
 function increaseCheese() {
   cheese += 1
   if (clickUpgrades[0].quantity > 0) {
@@ -45,56 +44,8 @@ function increaseCheese() {
   if (clickUpgrades[1].quantity > 0) {
     cheese += clickUpgrades[1].bonus * clickUpgrades[1].quantity
   }
-
   drawCheese()
 }
-
-
-
-
-
-//FIXME - In all buy functions make the price of the upgrade increase
-function buyKnife() {
-  if (cheese >= clickUpgrades[0].price) {
-    clickUpgrades[0].quantity += 1
-    // clickUpgrades[0].price += 1
-    cheese -= clickUpgrades[0].price
-    drawKnives()
-    drawCheese()
-  }
-}
-
-function buyDrill() {
-  if (cheese >= clickUpgrades[1].price) {
-    clickUpgrades[1].quantity += 1
-    // clickUpgrades[1].price += 1
-    //FIXME - add new line similar to above to increase price +1, for all buy functions
-    cheese -= clickUpgrades[1].price
-    drawDrills()
-    drawCheese()
-  }
-}
-
-function buyMousetronaut() {
-  if (cheese >= autoUpgrades[0].price) {
-    autoUpgrades[0].quantity += 1
-    // autoUpgrades[0].price += 1
-    cheese -= autoUpgrades[0].price
-    drawMousetronauts()
-    drawCheese()
-  }
-}
-
-function buySpaceStation() {
-  if (cheese >= autoUpgrades[1].price) {
-    autoUpgrades[1].quantity += 1
-    // autoUpgrades[1].price += 1
-    cheese -= autoUpgrades[1].price
-    drawSpaceStations()
-    drawCheese()
-  }
-}
-
 
 // FIXME - Do not apply the auto upgrades here - For loop is best solution here (but not needed)
 // Move this below logic to a collect auto function
@@ -110,7 +61,73 @@ function collectAuto() {
   drawCheese()
 }
 
+// NOTE - this isn't working
+function drawClickBonus() {
+  let clickBonus = (clickUpgrades[0].bonus * clickUpgrades[0].quantity + clickUpgrades[1].bonus * clickUpgrades[1].quantity)
+  const increaseClickBonusElm = document.getElementById('clickBonus')
+  increaseClickBonusElm.innerText = clickBonus.toString()
+}
+
+function drawAutoBonus() {
+  let autoBonus = (autoUpgrades[0].bonus * autoUpgrades[0].quantity + autoUpgrades[1].bonus * autoUpgrades[1].quantity)
+  console.log(autoBonus)
+  const increaseClickBonusElm = document.getElementById('autoBonus')
+  increaseClickBonusElm.innerText = autoBonus.toString()
+}
+
+
 //#endregion
+
+
+
+
+//FIXME - In all buy functions make the price of the upgrade increase
+function buyKnife() {
+  if (cheese >= clickUpgrades[0].price) {
+    clickUpgrades[0].quantity += 1
+    cheese -= clickUpgrades[0].price
+    clickUpgrades[0].price += 1
+    drawKnives()
+    drawCheese()
+    drawClickBonus()
+  }
+}
+
+function buyDrill() {
+  if (cheese >= clickUpgrades[1].price) {
+    clickUpgrades[1].quantity += 1
+    //FIXME - add new line similar to above to increase price +1, for all buy functions
+    cheese -= clickUpgrades[1].price
+    clickUpgrades[1].price += 1
+    drawDrills()
+    drawCheese()
+    drawClickBonus()
+  }
+}
+
+function buyMousetronaut() {
+  if (cheese >= autoUpgrades[0].price) {
+    autoUpgrades[0].quantity += 1
+    cheese -= autoUpgrades[0].price
+    autoUpgrades[0].price += 1
+    drawMousetronauts()
+    drawCheese()
+    drawAutoBonus()
+  }
+}
+
+function buySpaceStation() {
+  if (cheese >= autoUpgrades[1].price) {
+    autoUpgrades[1].quantity += 1
+    cheese -= autoUpgrades[1].price
+    autoUpgrades[1].price += 1
+    drawSpaceStations()
+    drawCheese()
+    drawAutoBonus()
+  }
+}
+
+
 
 
 
@@ -120,10 +137,7 @@ function drawCheese() {
   increaseCheeseElm.innerText = cheese.toString()
 }
 
-// function drawClickBonus() {
-//   const increaseClickBonusElm = document.getElementById('clickBonus')
-//   increaseClickBonusElm.innerText = 
-// }
+
 // FIXME - create a new variable ('let') and total up the upgrades. 
 
 // function drawAutoBonus() {
@@ -135,11 +149,16 @@ function drawCheese() {
 function drawKnives() {
   const increaseKnivesElm = document.getElementById('knifeTotal')
   increaseKnivesElm.innerText = clickUpgrades[0].quantity.toString()
+  const knivesPriceElm = document.getElementById('knifeCost')
+  knivesPriceElm.innerText = clickUpgrades[0].price.toString()
 }
 
 function drawDrills() {
   const increaseDrillsElm = document.getElementById('drillTotal')
   increaseDrillsElm.innerText = clickUpgrades[1].quantity.toString()
+  const drillsPriceElm = document.getElementById('drillCost')
+  drillsPriceElm.innerText = clickUpgrades[1].price.toString()
+
   //FIXME - add another ID on page called "drill price" 
   // get the drill price element by it's ID
   // change the drill price elements innertext to the price of the drill object
@@ -148,11 +167,15 @@ function drawDrills() {
 function drawMousetronauts() {
   const increaseMousetronautsElm = document.getElementById('mousetronautTotal')
   increaseMousetronautsElm.innerText = autoUpgrades[0].quantity.toString()
+  const mousetronautsPriceElm = document.getElementById('mousetronautCost')
+  mousetronautsPriceElm.innerText = autoUpgrades[0].price.toString()
 }
 
 function drawSpaceStations() {
   const increaseSpaceStationsElm = document.getElementById('spaceStationTotal')
   increaseSpaceStationsElm.innerText = autoUpgrades[1].quantity.toString()
+  const spaceStationsPriceElm = document.getElementById('spaceStationCost')
+  spaceStationsPriceElm.innerText = autoUpgrades[1].price.toString()
 }
 
 setInterval(collectAuto, 3000);
